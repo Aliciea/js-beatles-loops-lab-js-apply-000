b225128ee7bf5b6c7e9e9e98f7f0e188f77b3019
Aliciea D Suechin <adr330@nyu.edu>
2019-06-03 02:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   
   let array = [];
   
-  for(let counter = 0; counter < musicians.length; counter++){
+  for(let i = 0; i < musicians.length; i++){
     let str1 = musicians[i]; 
     let str2 = instruments[i];
     array.push(musicians[i] + `${plays}` + instruments[i]);
