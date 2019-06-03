9b50118a3a5656055f150f902e965628a42ba086
Aliciea D Suechin <adr330@nyu.edu>
2019-06-03 01:59:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   
   var array = [];
   
-  for(var counter = 0; counter < musicians.length; counter++){
+  for(let counter = 0; counter < musicians.length; counter++){
     let str1 = musicians[i] 
     let str2 = instruments[i]
     array.push(musicians[i]+ "John Lennon plays guitar" + instruments[i]);
