56a518b2cc81546f2a40ea4c585493dc0273809c
Aliciea D Suechin <adr330@nyu.edu>
2019-06-03 02:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function johnLennonFacts(arr) {
 function iLoveTheBeatles(num) {
   let arr = [];
   
-  do {arr.push("I love the Beatles!") num++;
-    while
-  }
+  do {arr.push("I love the Beatles!") 
+  num++;
+    } while
 }
 
