a5809d84db39fde7bbb16a3637760a3a06ed6a16
Aliciea D Suechin <adr330@nyu.edu>
2019-06-03 01:36:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   let array = [''];
   
   for(let counter = 0; counter < musicians.length; counter++){
-    array.push(musicians[i]); 
+    array.push(musicians[i]) + `"John Lennon plays guitar"` + 
   }
 }
 
