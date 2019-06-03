f2797be9a62b5b1bd991f1af4e9741fa67ba08db
Aliciea D Suechin <adr330@nyu.edu>
2019-06-03 01:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   
-  let array = [];
+  var array = [];
   
-  for(let counter = 0; counter < musicians.length; counter++){
+  for(var counter = 0; counter < musicians.length; counter++){
     array.push(musicians[i] + `plays` + instruments[i]);
   }
   return array;
